2179731b613862cde734290b00e78c42b28bc33f
Rohan Agarwal <21052610@kiit.ac.in>
2024-06-29 09:43:38 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import { ThemeProvider } from "styled-components";
 import { lightTheme } from "./utils/Themes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useState } from "react";
-
 import Authentication from "./pages/Authentication";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Workouts from "./pages/Workouts";
-
 import styled from "styled-components";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { FoodCalories } from "./Context/foodContext";
+import { useState } from "react";
+import Contact from "./pages/Contact";
+import Tutorial from "./pages/Tutorial";
+import ExerciseDetails from "./pages/ExerciseDetails";
+
 
 const Container = styled.div`
   width: 100%;
@@ -18,31 +22,39 @@ const Container = styled.div`
   flex-direction: column;
   background: ${({ theme }) => theme.bg};
   color: ${({ theme }) => theme.text_primary};
-  overflow: hidden; /* Updated to a single overflow property */
+  // overflow: hidden; /* Updated to a single overflow property */
   transition: all 0.2s ease;
 `;
 
 function App() {
-  const { currentUser } = useSelector((state) => state.user);
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+  const [nutritions, setNutritions] = useState([]);
+  localStorage.setItem('nutritions', JSON.stringify(nutritions));
 
   return (
-    <ThemeProvider theme={lightTheme}>
-      <BrowserRouter>
-        <Container>
-          {currentUser ? (
-            <>
-              <Navbar currentUser={currentUser} />
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/workouts" element={<Workouts />} />
-              </Routes>
-            </>
-          ) : (
-            <Authentication />
-          )}
-        </Container>
-      </BrowserRouter>
-    </ThemeProvider>
+    <FoodCalories.Provider value={{ nutritions, setNutritions }}>
+      <ThemeProvider theme={lightTheme}>
+        <BrowserRouter>
+          <ToastContainer position="bottom-center" />
+          <Container>
+            {userInfo ? (
+              <>
+                <Navbar currentUser={userInfo} />
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/workouts" element={<Workouts />} />
+                  {/* /* <Route path="/tutorials" element={<Tutorial />} /> */ }
+                  <Route path="/contact" element={<Contact />} />
+                 {/* /* <Route path="/exercise/:id" element={<ExerciseDetails />} /> /* */}
+                </Routes>
+              </>
+            ) : (
+              <Authentication />
+            )}
+          </Container>
+        </BrowserRouter>
+      </ThemeProvider>
+    </FoodCalories.Provider>
   );
 }
 
