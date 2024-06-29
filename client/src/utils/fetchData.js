bef28b1ae7208ea4508fc5987112233e04768b23
Rohan Agarwal <21052610@kiit.ac.in>
2024-06-29 09:48:55 +0530

    Add files via upload

---
+++ client/src/utils/fetchData.js
@@ -0,0 +1,14 @@
+// export const exerciseOptions = {
+	//method: 'GET',
+	//headers: {
+		//'x-rapidapi-key': 'c92e06d5c6mshfce3e7cdfaad84ap1f19dfjsn2f60255af180',
+		//'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+	//}
+// }; 
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
